fe1420115a1463cf762f805d8f838e0fe86db2ac
Sabbir Al Azim Shehab <56802016+sabbir421@users.noreply.github.com>
2021-05-11 05:44:53 +0600

    Add files via upload

---
+++ class.js
@@ -0,0 +1,12 @@
+class Player {
+    constructor(name, match, run, fifty, hundred){
+        this.name=name;
+        this. match= match;
+        this.run = run;
+        this.fifty = fifty;
+        this.hundred= hundred;
+    }
+}
+const details = new Player("Tamim", 50, 2000, 15 , 5);
+document.write(details);
+console.log(details); 
