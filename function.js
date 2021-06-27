da1bbadf404993338c4efd6e131ed5c0cdb94ab3
Sabbir Al Azim Shehab <56802016+sabbir421@users.noreply.github.com>
2021-06-27 11:42:37 -0700

    i'm used this function for calculate simple add , sub , mul  and div

---
+++ function.js
@@ -0,0 +1,31 @@
+function add (num1 , num2)
+{
+    var result=num1+num2;
+    console.log(result);
+}
+
+
+function sub (num1 , num2)
+{
+    var result=num1-num2;
+    console.log(result);
+}
+
+
+function mul (num1 , num2)
+{
+    var result=num1*num2;
+    console.log(result);
+}
+
+
+function div (num1 , num2)
+{
+    var result=num1/num2;
+    console.log(result);
+}
+
+add(5,10),
+sub(10,6);
+mul(3,5),
+div(10 ,5);
